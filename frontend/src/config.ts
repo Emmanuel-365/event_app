4af09025b2994026aa4391c1a76f595ac037bf0a
Noubissie237 <wilfried.noubissie@facsciences-uy1.cm>
2025-12-20 15:43:31 +0100

    Moving .env

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,5 @@
 // src/config.ts
-const VITE_API_URL = 'http://62.84.186.220:8080';
+const VITE_API_URL = 'https://api.jamaa.site';
 
 export const API_BASE_URL = `${VITE_API_URL}/api`;
 export const AUTH_BASE_URL = VITE_API_URL;
